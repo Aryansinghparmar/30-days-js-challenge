4cc873f4dfb30c1815d6c3e7246aa2b924e5d7ac
Aryansinghparmar <152505734+Aryansinghparmar@users.noreply.github.com>
2024-07-23 17:44:03 +0000

    js day1 challenge complete

--- DAY1.js
+++ DAY1.js
@@ -37,13 +37,49 @@ const Myself = {
 let luckyNum = 7
 luckyNum = 69
 
-console.log(luckyNum);
+// console.log(luckyNum);
 
 /*Activity 5*/ 
 
 // Task 6:
 
 const LuckyNum = 7
-LuckyNum = 69
+// LuckyNum = 69
 
-console.log(LuckyNum);//TypeError: Assignment to constant variable.
+// console.log(LuckyNum);//TypeError: Assignment to constant variable.
+
+// Feature Request
+
+// 1
+let value = 100
+console.log(value);
+console.log(typeof (value));
+
+let booleanNew = true
+console.log(booleanNew);
+console.log(typeof boolean);
+
+let array1 = [1,2,3,4]
+console.log(array1);
+console.log(typeof array1);
+
+const NewObj = {
+    college: "IIT Kharagpur",
+    Departement: "Mining Engineering"
+}
+
+console.log(NewObj);
+console.log(typeof NewObj);
+
+const tempFunction = function squareofNum (num) {
+        squr =  (num * num);
+        return squr;
+}
+
+console.log(tempFunction(8));
+console.log(typeof (tempFunction));
+
+
+// 2
+// the difference between let and const is that when we want to reassign a value to a variable which is defined using let we can do it but when we define a variable using const then we can not assign it any new value  
+
