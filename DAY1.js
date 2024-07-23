cc98ca6b87b01bf9973ca0f7bd24862f58d91f9a
Aryansinghparmar <152505734+Aryansinghparmar@users.noreply.github.com>
2024-07-23 04:23:49 +0000

    Day1 Challenge Complete

---
+++ DAY1.js
@@ -0,0 +1,49 @@
+/*Activity 1*/ 
+
+// Task 1:
+var RandomVar = 69
+// console.log(RandomVar);
+
+// Task 2:
+let string = "Aaryan is a good guy"
+// console.log(string);
+
+/*Activity 2*/ 
+
+// Task 3:
+const boolean = true
+// console.log(boolean);
+
+/*Activity 3*/ 
+
+// Task 4:
+let arr = [1,2,3,4]
+// console.log(typeof(arr));
+
+const Myself = {
+    name: "aaryan",
+    age: 19,
+    sex: "male",
+
+
+}
+
+// console.log(Myself);
+// console.log(typeof(Myself));
+
+/*Activity 4*/ 
+
+// Task 5:
+let luckyNum = 7
+luckyNum = 69
+
+console.log(luckyNum);
+
+/*Activity 5*/ 
+
+// Task 6:
+
+const LuckyNum = 7
+LuckyNum = 69
+
+console.log(LuckyNum);//TypeError: Assignment to constant variable.
