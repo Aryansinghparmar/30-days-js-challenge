416782a17a973d2aaa9c38068bd1fa83fdd45494
Aryansinghparmar <152505734+Aryansinghparmar@users.noreply.github.com>
2024-07-24 01:22:40 +0000

    Day2 JS Challenge Complete

---
+++ DAY2.js
@@ -0,0 +1,67 @@
+// Activity 1: Arithmetic Operation
+
+// Task 1:
+ function sum(num1, num2) {
+    return num1 + num2
+}
+
+// console.log(sum(5,69));
+
+// Task 2:
+function sub(num1, num2) {
+    return num1 - num2
+}
+
+// console.log(sub(5,69));
+
+// Task 3:
+function Multiply(num1, num2) {
+    return num1 * num2
+}
+
+// console.log(Multiply(5,69));
+
+// Task 4:
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+// console.log(divide(5,69));
+
+// Task 5:
+function Remainder(num1, num2) {
+    return num1 % num2
+}
+
+// console.log(Remainder(5,69));
+
+// Assignment Operators
+
+// Task 6:
+
+let NewSum = 69
+NewSum += 9
+// console.log(NewSum);
+
+// Task 7:
+
+let NewSub = 69
+NewSub -= 9
+// console.log(NewSub);
+
+// Activity 3: Comparison Operators
+
+// Task 8:
+
+function Campare(x, y) {
+    if (x > y) {
+      return  console.log(`Number ${x} is greater than Number ${y}`);
+    }else if (y > x) {
+     return   console.log(`Number ${y} is greater than Number ${x}`);
+     } else {
+      return  console.log(`Both Numbers are same`);
+    }
+}
+
+console.log(Campare(5, 40));
+
